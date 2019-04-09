864a020bb1ee2723d76f836d5d70fd288229782b
krprls <krprls@gmail.com>
2019-04-09 00:11:59 -0400

    Add files via upload

---
+++ EmployeePay.py
@@ -0,0 +1,18 @@
+#dictionariesfor records employee, pay rate, time,ArithmeticError etc.,
+#recordsPay = {}
+
+#Ziplist
+#input excel as pd
+#read the table and use function
+
+#function toc caliculate the pay
+
+def emplpay(payRate, hoursWorked):
+    if hoursWorked > 40:
+        pay40 = payRate * 40
+        extraPay = (1.5 * payRate) * (hoursWorked - 40)
+        totalpay = pay40 + extraPay
+        otherwise
+        totalPay = payRate * hoursWorked
+      print out name, salary#
+    
